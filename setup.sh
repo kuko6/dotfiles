5e7e3a15e4956176c7c4c88541225c1e91572571
Jakub Povinec <jakub.povinec@gmail.com>
2025-08-08 08:29:47 +0200

    updated setup script

--- setup.sh
+++ setup.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-# remove any existing symlinks/directories
-rm -rf ~/.config/helix ~/.config/nvim ~/.config/btop ~/.config/tmux
+# https://github.com/caarlos0/dotfiles/blob/main/setup
+set -euo pipefail
 
-# create fresh symlinks
-ln -sf "$(pwd)/helix" ~/.config/helix
-ln -sf "$(pwd)/nvim" ~/.config/nvim
-ln -sf "$(pwd)/btop" ~/.config/btop
-ln -sf "$(pwd)/tmux" ~/.config/tmux
+link() {
+  mkdir -p "$(dirname "$2")"
+  if [ -e "$2" ]; then
+    echo "Skipping $2, already exists."
+  else
+    ln -sf "$1" "$2"
+    echo "Linked $1 to $2"
+  fi
+}
+
+# create symlinks
+link "$(pwd)/helix" ~/.config/helix
+link "$(pwd)/nvim" ~/.config/nvim
+link "$(pwd)/btop" ~/.config/btop
+link "$(pwd)/tmux" ~/.config/tmux
